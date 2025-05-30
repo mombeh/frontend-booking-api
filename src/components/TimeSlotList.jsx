990f7d7a31dda7fcd615d205d2d673f7c46e7ff0
mombeh nadine <nadinemombeh@gmail.com>
2025-05-30 14:07:19 +0100

    formatted my code

--- src/components/TimeSlotList.jsx
+++ src/components/TimeSlotList.jsx
@@ -15,18 +15,18 @@ const TimeSlotList = ({ slots = [], error = '' }) => {
   if (slots.length === 0) return <p>No time slots created yet.</p>;
 
   return (
-<section className="time-slot-section">
-  <h3>Your Time Slots:</h3>
-  <ul className="time-slot-list">
-    {slots.map((slot) => (
-      <li className="time-slot-item" key={slot._id || slot.id}>
-        <span className="date">{new Date(slot.date).toLocaleDateString()}</span>
-        {' | '}
-        <span className="time">{formatTime(slot.startTime)} - {formatTime(slot.endTime)}</span>
-      </li>
-    ))}
-  </ul>
-</section>
+    <section className="time-slot-section">
+      <h3>Your Time Slots:</h3>
+      <ul className="time-slot-list">
+        {slots.map((slot) => (
+          <li className="time-slot-item" key={slot._id || slot.id}>
+            <span className="date">{new Date(slot.date).toLocaleDateString()}</span>
+            {' | '}
+            <span className="time">{formatTime(slot.startTime)} - {formatTime(slot.endTime)}</span>
+          </li>
+        ))}
+      </ul>
+    </section>
 
   );
 };
